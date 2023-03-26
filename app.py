cdcdcbfdce7e66ebb35a9fed38bed91dc6c58b63
Aanchal mahajan <98211852+aanchalmahajan3112@users.noreply.github.com>
2023-03-26 18:59:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def predict():
     return render_template('index.html', prediction_text='Your Rating is: {}'.format(output))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=8080)
